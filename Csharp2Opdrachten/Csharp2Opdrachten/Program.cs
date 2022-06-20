a348969e37ba42e533070fbc963b0da6eca4b260
10KJVN <Jahva_game11@hotmail.com>
2022-06-20 15:13:09 +0200

    Files en directories bronze

--- Csharp2Opdrachten/Csharp2Opdrachten/Program.cs
+++ Csharp2Opdrachten/Csharp2Opdrachten/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
-namespace Csharp2Opdrachten
+using System.IO;
+namespace FileDirOpdracht
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Jahva\MA\f1m4\GD\2022P4csharp\Csharp2Opdrachten\Csharp2Opdrachten");
+            Console.WriteLine("No search pattern returns:");
+            foreach (DirectoryInfo fil in dir.GetDirectories())
+            {
+                Console.WriteLine(fil.Name);
+            }
+
+            Console.WriteLine();
+
+            foreach (FileInfo fil in dir.GetFiles())
+            {
+                Console.WriteLine(fil.Name);
+            }
+
         }
     }
 }
