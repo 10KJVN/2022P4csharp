b1d91f6789a96c0aeedde7214742cab5699cc282
10KJVN <Jahva_game11@hotmail.com>
2022-06-24 12:04:49 +0200

    FileDir en File IO opdrachten bronze, stonden in de verkeerde directory.

---
+++ Csharp2Opdrachten/FileIOOpdracht/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace FileIOOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Text = File.ReadAllLines(@"C:\Users\Jahva\MA\f1m4\2022P4csharp\Csharp2Opdrachten\FileIOOpdracht\iotext.txt");
+
+            foreach (string Line in Text)
+                Console.WriteLine(Line);
+        }
+    }
+}
