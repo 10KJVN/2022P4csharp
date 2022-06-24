9eede145b2174fc81fe74ed99c92c2585831dba7
10KJVN <Jahva_game11@hotmail.com>
2022-06-24 12:41:17 +0200

    String opdracht bronze, afgetekend

---
+++ StringOpdracht/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace FileIOOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringText = File.ReadAllText(@"C:\Users\Jahva\MA\f1m4\GD\2022P4csharp\StringOpdracht\stringsplit.txt");
+            string[] text = stringText.Split(':', ',');
+
+            foreach (string tekst in text)
+            {
+                Console.WriteLine(tekst);
+            }
+        }
+    }
+}
